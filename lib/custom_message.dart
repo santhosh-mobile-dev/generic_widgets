918a9bdc3440848c84cf6a8d99b3fc6469d9945a
Komal <komal.offsureit@gmail.com>
2020-06-12 22:11:03 +0530

    Created Create Account screen
    Worked on Create Account screen and created textLabel generic widget

---
+++ lib/custom_message.dart
@@ -0,0 +1,11 @@
+class CustomMessages {
+
+  static String loginText = "Login";
+  static String createAccount = "Create Account";
+  static String name = "Name";
+  static String emailAddress = "Email Address";
+  static String password = "Password";
+
+
+  static String alreadyHaveAnAccount = "Already have an account?";
+}
