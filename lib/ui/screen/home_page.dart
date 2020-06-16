fe135b0ea05d9371c0742231795c6382ae624a0c
Ranjini K R <ranjini@aeione.com>
2020-06-16 11:50:38 +0530

    added side menu
    closes #8

--- lib/ui/screen/home_page.dart
+++ lib/ui/screen/home_page.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:genericwidgetapp/ui/screen/custom_bottom_bar.dart';
-import 'package:genericwidgetapp/ui/screen/profile_screen.dart';
-import 'package:genericwidgetapp/ui/screen/wallet_screen.dart';
-
+import 'package:genericwidgetapp/ui/screen/home_drawer.dart';
 import 'dashboard_screen.dart';
 import 'notifications_screen.dart';
+import 'profile_screen.dart';
+import 'wallet_screen.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
   final List _children = [
@@ -22,22 +21,22 @@ class _HomePageState extends State<HomePage>
     ProfileScreen()
   ];
 
+  Widget screenView(BuildContext context) {
+    return Center(child: _children[_currentIndex]);
+  }
+
   Widget appBar() {
     return AppBar(
       title: Text('Home'),
       centerTitle: true,
-      automaticallyImplyLeading: false,
     );
-  }
-
-  Widget screenView(BuildContext context) {
-    return Center(child: _children[_currentIndex]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      drawer: HomeDrawer(),
       bottomNavigationBar: CustomBottomNavigationBar(),
       body: screenView(context),
     );
